34fa6734d66786e553bcec6e5d27c7b5f33c0f63
SonuToor <savrooptoor92@gmail.com>
2019-08-08 22:26:50 -0400

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Navigation from "./Components/Navigation"
 
-export default App;
+export default class App extends React.Component {
+  constructor(){
+    super()
+  }
+  render() {
+    return (
+      <div>      
+        <Navigation/>
+        <h1>Travel Planner</h1>
+      </div>
+    )
+  }
+}
