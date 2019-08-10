98f113482ef134852ba7d7d62359030e325195ba
SonuToor <savrooptoor92@gmail.com>
2019-08-09 21:40:02 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 
 import Navigation from "./Components/Navigation"
 
+
 export default class App extends React.Component {
   constructor(){
     super()
+    this.state = {
+      loggedIn : false,
+      userName : null
+    }
   }
   render() {
     return (
       <div>      
-        <Navigation/>
+        <Navigation loggedIn={this.state.loggedIn}/>
         <h1>Travel Planner</h1>
       </div>
     )
