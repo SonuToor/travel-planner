80d1031ed6cb0807a37892b315f6e09860932972
SonuToor <savrooptoor92@gmail.com>
2019-10-18 14:24:56 -0400

    refactored the rendering of each individual day on itinerary. Abstracted a component away from Day.js to make code more readable.

---
+++ src/Components/homecomponents/itinerarycomponents/PeriodOfDayCard.js
@@ -0,0 +1,27 @@
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+import React from 'react';
+
+
+export default function PeriodOfDayCard (props)  {
+    return (
+        <Card className={props.class}>
+            <CardContent>
+                <h3 className="card-title">{props.period}</h3>
+                <ul className="activities-list">
+                    {props.list.map(activity => 
+                        <li key={activity}>
+                            {activity}
+                            <IconButton id={activity} onClick={e => props.handleDelete(e, activity)}>
+                                <DeleteIcon id={activity} fontSize="small" />
+                            </IconButton>
+                        </li>
+                    )}
+                </ul>
+            </CardContent>
+        </Card>
+
+    )
+}
